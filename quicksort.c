7e83bb291718d34305ea74e2d83bd21e1355e8c9
Arnav Satish Gawas <asg7201@rit.edu>
2025-12-04 14:22:20 -0500

    implemented unthreaded quicksort

--- quicksort.c
+++ quicksort.c
@@ -2,6 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
+int *quicksort(size_t size, const int *data) {
+    if (size == 0) return NULL;
+    if (size == 1) {
+        int *res = malloc(sizeof(int));
+        res[0] = data[0];
+        return res;
+    }
+
+    int pivot = data[0];
+    size_t less_count = 0, same_count = 0, more_count = 0;
+
+    for (size_t i = 0; i < size; i++) {
+        if (data[i] < pivot) less_count++;
+        else if (data[i] > pivot) more_count++;
+        else same_count++;
+    }
+
+    int *less = malloc(less_count * sizeof(int));
+    int *more = malloc(more_count * sizeof(int));
+    size_t l = 0, m = 0;
+
+    for (size_t i = 0; i < size; i++) {
+        if (data[i] < pivot) less[l++] = data[i];
+        else if (data[i] > pivot) more[m++] = data[i];
+    }
+
+    int *sorted_less = quicksort(less_count, less);
+    int *sorted_more = quicksort(more_count, more);
+
+    int *result = malloc(size * sizeof(int));
+    size_t idx = 0;
+
+    for (size_t i = 0; i < less_count; i++) result[idx++] = sorted_less[i];
+    for (size_t i = 0; i < same_count; i++) result[idx++] = pivot;
+    for (size_t i = 0; i < more_count; i++) result[idx++] = sorted_more[i];
+
+    free(less);
+    free(more);
+    free(sorted_less);
+    free(sorted_more);
+
+    return result;
+}
+
 int main(int argc, char *argv[]) {
     int print_flag = 0;
     char *filename = NULL;
@@ -22,9 +66,9 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    int capacity = 4;
+    size_t capacity = 4;
     int *arr = malloc(capacity * sizeof(int));
-    int n = 0;
+    size_t n = 0;
 
     while (1) {
         int x;
@@ -35,18 +79,31 @@ int main(int argc, char *argv[]) {
         }
         arr[n++] = x;
     }
-
     fclose(fp);
 
     if (print_flag) {
-        for (int i = 0; i < n; i++) {
+        printf("Unsorted list before non-threaded quicksort: ");
+        for (size_t i = 0; i < n; i++) {
             printf("%d", arr[i]);
             if (i < n - 1) printf(",");
         }
         printf("\n");
     }
 
+    int *sorted = quicksort(n, arr);
+
+    if (print_flag) {
+        printf("Resulting list: ");
+        for (size_t i = 0; i < n; i++) {
+            printf("%d", sorted[i]);
+            if (i < n - 1) printf(",");
+        }
+        printf("\n");
+    }
+
     free(arr);
+    free(sorted);
+
     return 0;
 }
 
